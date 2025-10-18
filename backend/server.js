4c17d0d83acf6aea1eeb6da7ffe468df8ae7c94f
Raghav verma <146425978+Raghavverma109@users.noreply.github.com>
2025-10-19 04:25:45 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -12,10 +12,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(cors({
-  origin: [
-    "http://localhost:5173",
-    "https://vote-frontend.vercel.app",
-  ],
+  origin:"https://vote-frontend.vercel.app",
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
@@ -40,4 +37,5 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+
 
